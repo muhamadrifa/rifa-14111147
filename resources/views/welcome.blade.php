77f91e9adf78571c1ab449b6ab8740e41acc1e16
muhamadrifa <muhamadrifa206@gmail.com>
2017-02-15 21:47:00 +0700

    update file baru si rifaaa hahaha

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 created by muhamad rifaaa</div>
             </div>
         </div>
     </body>
